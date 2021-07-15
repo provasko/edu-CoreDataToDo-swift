e03e380d4d340146b79ac0a1c810002f18489043
provasko <80074452+provasko@users.noreply.github.com>
2021-07-15 11:46:57 +0200

    Edit function added

--- CoreDataToDo/ViewController.swift
+++ CoreDataToDo/ViewController.swift
@@ -59,10 +59,38 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
     }
     
-    
-    
-    
-    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let item = models[indexPath.row]
+        
+        let sheet = UIAlertController(title: "Edit", message: nil, preferredStyle: .actionSheet)
+        
+        sheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        sheet.addAction(UIAlertAction(title: "Edit", style: .default, handler: { _ in
+            
+            let alert = UIAlertController(title: "Edit Item", message: "Edit your item", preferredStyle: .alert)
+            
+            alert.addTextField(configurationHandler: nil)
+            alert.textFields?.first?.text = item.name
+            alert.addAction(UIAlertAction(title: "Save", style: .cancel, handler: { [weak self] _ in
+                guard let field = alert.textFields?.first, let newName = field.text, !newName.isEmpty else{
+                    return
+                }
+                
+                self?.updateItem(item: item, newName: newName)
+                
+            }))
+            
+            self.present(alert, animated: true)
+            
+        }))
+        sheet.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: { [weak self] _ in
+            self?.deleteItem(item: item)
+        }))
+        
+        present(sheet, animated: true)
+        
+    }
     
     // Core Data
     
@@ -101,20 +129,20 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         do {
             try context.save()
-            
+            getAllItems()
         }
         catch {
             
         }
     }
     
-    func utdateItem(item: ToDoListItem, newName: String) {
+    func updateItem(item: ToDoListItem, newName: String) {
 
         item.name = newName
         
         do {
             try context.save()
-            
+            getAllItems()
         }
         catch {
             
