f226f2df8f16356c9b2dff12baa036610c065058
provasko <80074452+provasko@users.noreply.github.com>
2021-07-15 10:14:59 +0200

    Add button created, works. List is saving

--- CoreDataToDo/ViewController.swift
+++ CoreDataToDo/ViewController.swift
@@ -23,18 +23,38 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         super.viewDidLoad()
         title = "To Do List"
         view.addSubview(tableView)
+        getAllItems()
         tableView.delegate = self
         tableView.dataSource = self
         tableView.frame = view.bounds
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(didTapAdd))
+    }
+    
+    @objc private func didTapAdd() {
+        let alert = UIAlertController(title: "New Item", message: "Enter new item", preferredStyle: .alert)
+        
+        alert.addTextField(configurationHandler: nil)
+        alert.addAction(UIAlertAction(title: "Submit", style: .cancel, handler: { [weak self] _ in
+            guard let field = alert.textFields?.first, let text = field.text, !text.isEmpty else{
+                return
+            }
+            
+            self?.createItem(name: text)
+            
+        }))
+        
+        present(alert, animated: true)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return models.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = models[indexPath.row]
         let cell  = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "Hello World"
+        cell.textLabel?.text = model.name
         return cell
         
     }
@@ -49,6 +69,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func getAllItems() {
         do{
             models = try context.fetch(ToDoListItem.fetchRequest())
+            
+            DispatchQueue.main.async{
+                self.tableView.reloadData()
+            }
         }
         catch {
             //error
@@ -63,7 +87,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         do {
             try context.save()
-            
+            getAllItems()
         }
         catch {
             
